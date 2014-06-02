bc863bd32efdf244045f9138cdc33cf8dbc0ea6d
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2014-06-02 16:02:50 -0300

    minor bug fix

--- askbot_log/middleware/watch_users.py
+++ askbot_log/middleware/watch_users.py
@@ -4,8 +4,8 @@ from askbot_log.models import UserLog
 
 class LogActivityOfWatchedUsers(object):
     def process_response(self, request, response):
-        user = request.user
-        if request.method == 'POST' and user.is_authenticated() and user.is_watched():
+        user = getattr(request, 'user', None)
+        if user and request.method == 'POST' and user.is_authenticated() and user.is_watched():
             log = UserLog()
             log.ip_addr = request.META['REMOTE_ADDR']
             log.user = user
